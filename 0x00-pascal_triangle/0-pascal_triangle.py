c26c3c030678f40e7ad9aa57162eca2332b4d779
Naod Taddese <naodtds@gmail.com>
2024-05-30 09:20:23 +0300

    pascals triangle done

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""pascals triangle list"""
+
+def pascal_triangle(n):
+    '''function to generate pascals triangle'''
+    if n > 0:
+        tri = [[1]]
+
+        for i in range(1, n):
+            row = [1]
+            for j in range(1, i):
+                row.append(tri[i-1][j-1] + tri[i-1][j])
+            row.append(1)
+            tri.append(row)
+        return tri
+    else:
+        return []
