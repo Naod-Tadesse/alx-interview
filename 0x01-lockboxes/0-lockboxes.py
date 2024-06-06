b24c51bd9171467f0e07f2529e2ca785282dfbf2
Naod Taddese <naodtds@gmail.com>
2024-06-06 22:33:19 +0300

    fix

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -6,19 +6,19 @@ from typing import List
 
 def canUnlockAll(bs: List[List[int]]) -> bool:
     """lockbox checker"""
-    if not bs:
+    if bs:
+        num = len(bs)
+        visited = [False] * num
+        keys = [0]
+
+        while keys:
+            box = keys.pop()
+            visited[box] = True
+
+            for key in bs[box]:
+                if key < num and not visited[key]:
+                    keys.append(key)
+
+        return all(visited)
+    else:
         return False
-
-    n = len(bs)
-    visited = [False] * n
-    keys = [0]
-
-    while keys:
-        box = keys.pop()
-        visited[box] = True
-
-        for key in bs[box]:
-            if key < n and not visited[key]:
-                keys.append(key)
-
-    return all(visited)
